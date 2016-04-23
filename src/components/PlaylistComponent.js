3dbce41eb7c1717ae1e03393410f8623043c4e55
Justin Conway <justin@conwaydev.com>
2016-04-23 12:52:54 -0500

    Initial commit scaffolded by generator-react-webpack-redux

---
+++ src/components/PlaylistComponent.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+
+require('styles//Playlist.scss');
+
+class PlaylistComponent extends React.Component {
+  render() {
+    return (
+      <div className="playlist-component">
+        Please edit src/components///PlaylistComponent.js to update this component!
+      </div>
+    );
+  }
+}
+
+PlaylistComponent.displayName = 'PlaylistComponent';
+
+// Uncomment properties you need
+// PlaylistComponent.propTypes = {};
+// PlaylistComponent.defaultProps = {};
+
+export default PlaylistComponent;
